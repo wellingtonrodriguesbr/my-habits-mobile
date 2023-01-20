83a9a720cee7c78ea58aac53a09c6c97b172f6c2
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-20 06:54:03 -0300

    feat: create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import colors from "tailwindcss/colors";
+import LogoImage from "../assets/logo.svg";
+
+export function Header() {
+  return (
+    <View className="w-full flex-row items-center justify-between">
+      <LogoImage />
+      <TouchableOpacity
+        activeOpacity={0.7}
+        className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+      >
+        <Feather name="plus" color={colors.violet[500]} size={20} />
+        <Text className="text-white text-base ml-3 font-semibold">Novo</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
