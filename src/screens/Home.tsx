e314469924dd5fdfc265ecdb4022e1ba95fa0ae3
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-20 06:54:15 -0300

    feat: create Home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,47 @@
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { DAY_SIZE, HabitDay } from "../components/HabitDay";
+import { Header } from "../components/Header";
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
+
+export function Home() {
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <Header />
+      <View className="flex-row justify-center items-center mt-6 mb-2">
+        {weekDays.map((day, index) => (
+          <Text
+            key={`${day}-${index}`}
+            className="font-bold text-zinc-400 font-xl mx-1 text-center"
+            style={{ width: DAY_SIZE }}
+          >
+            {day}
+          </Text>
+        ))}
+      </View>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((date) => (
+            <HabitDay key={date.toString()} />
+          ))}
+          {amountOfSDaystoFill > 0 &&
+            Array.from({ length: amountOfSDaystoFill }).map((_, index) => (
+              <View
+                key={index}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const datesFromYearStart = generateDatesFromYearBeginning();
+const minimumSummaryDatesSize = 18 * 5;
+const amountOfSDaystoFill = minimumSummaryDatesSize - datesFromYearStart.length;
