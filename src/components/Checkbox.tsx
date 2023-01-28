5ae0fc8af96d547b5fe7f3723211ac581f6ecd92
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-28 13:54:23 -0300

    style: update weight text

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -26,7 +26,7 @@ export function Checkbox({ title, checked = false, ...props }: CheckboxProps) {
       ) : (
         <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
       )}
-      <Text className="text-white ml-3">{title}</Text>
+      <Text className="text-white ml-3 font-semibold">{title}</Text>
     </TouchableOpacity>
   );
 }
