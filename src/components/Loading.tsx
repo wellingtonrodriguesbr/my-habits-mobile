26b3a05e6296deefcba54a9a4529d8c70dc830ef
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-31 06:07:50 -0300

    feat: create Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,16 @@
-import { Text, View } from "react-native";
+import { ActivityIndicator, View } from "react-native";
 
 export function Loading() {
   return (
-    <View>
-      <Text>Carregando ...</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#09090A",
+      }}
+    >
+      <ActivityIndicator color="#7C3AED" />
     </View>
   );
 }
